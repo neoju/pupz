ec60729fcea315bf56d2747455316e9e0ca01b1d
neo <neo01796@gmail.com>
2026-08-28 16:59:06 +0700

    chore: add app entry and bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
+if (import.meta.env.DEV) {
+  void import("react-grab");
+  void import("react-scan");
+}
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@fontsource-variable/geist";
+import "@fontsource-variable/roboto-condensed";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
